989ec558019afabedafa3ef1d003745341762991
Adeline R <adeline.rdev@gmail.com>
2022-07-04 09:45:43 +0200

    edde

--- src/container/ListContainer.jsx
+++ src/container/ListContainer.jsx
@@ -1,7 +1,20 @@
 import List from "../components/List";
+import { useState } from "react";
 
 const ListContainer = () => {
-  return <h1> Je suis le container </h1>;
+  const [value, setValue] = useState("");
+  const [lists, setLists] = useState([]);
+
+  function handleOnClick() {
+    setLists((prevState) => [...prevState, { id: 1, title: value }]);
+    setValue("");
+  }
+  return (
+    <>
+      <input value={value} onChange={(e) => setValue(e.target.value)} />
+      <button onClick={() => handleOnClick()}> Créer une liste </button>
+    </>
+  );
 };
 
 export default ListContainer;
